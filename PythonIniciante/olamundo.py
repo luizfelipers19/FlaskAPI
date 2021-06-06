caf8ae3c0003943eb1b20da602a610015dd80597
Luiz Felipe <luizfelipers19@gmail.com>
2021-06-05 23:18:43 -0300

    Added most of the variable types

--- PythonIniciante/olamundo.py
+++ PythonIniciante/olamundo.py
@@ -1,2 +1,53 @@
 #mais um HelloWorld simpático que todo curso apresenta
 print("Ola Mundo pela n-ésima vez")
+
+#definindo a = 3
+a = 3
+
+#printando o valor de a (valor esperado: 3)
+print(a)
+
+#modificando o valor de a para 4
+a = 4
+
+#printando o valor modificado de a (valor esperado: 4)
+print(a)
+
+#declarando b e definindo seu valor para 6
+b = 6
+
+#printando o valor de b
+print(b)
+
+#declarando C e definindo seu valor para um número quebrado/float
+c = 2.3
+print(c)
+
+#informando o tipo do valor das variáveis A, B e C
+print(type(a))
+print(type(b))
+print(type(c))
+
+#printando o valor das variáveis a partir da concatenação de strings e valores
+print("O valor de A é:", a)
+print("O valor de B é:", b)
+print("O valor de C é:", c)
+
+#printando o valor das 3 variáveis
+print("O valor de A é:",a,", o valor de B é:", b, "e o valor de C é:",c)
+
+#declarando variável string
+e = 'Sou uma string de aspas simples'
+print(e)
+
+f = "Sou uma string de aspas duplas"
+print(f)
+
+g = "Essa string usa aspas simples em um 'termo' contido nela"
+print(g)
+
+h = "Essa string está envolvida com aspas simples e usa a contra-barra \ para anular o \"fechamento\" das aspas duplas"
+print(h)
+
+#declarando variável que armazena valor booleano
+booleana = True
