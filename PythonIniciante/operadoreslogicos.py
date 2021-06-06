bb9eff217a9f9ee75abb7bc33ebfa03429f2d1f6
Luiz Felipe <luizfelipers19@gmail.com>
2021-06-05 23:58:21 -0300

    arithmetical and logic operators

---
+++ PythonIniciante/operadoreslogicos.py
@@ -0,0 +1,58 @@
+#Operadores Lógicos
+
+#Adição
+print('')
+print('Adição +')
+print("3+2=", 3+2)
+print("10+10=", 10+10)
+
+#subtração
+print('')
+print('Subtração -')
+print("10-6=", 10-6)
+print("2-3=",2-3)
+
+#multiplicação
+print('')
+print('Multiplicação *')
+print("10*2=", 10*2)
+print("3*2=", 3*2)
+
+#divisão
+print('')
+print('Divisão /')
+print("10/4=", 10/4)
+print("20/5=", 20/5)
+
+#Resto
+print('')
+print('Resto de divisão %')
+print("10%3=",10%3)
+print("5%3=", 5%3)
+
+#Potenciação
+print('')
+print('Potenciação **')
+print("10**3=",10**3)
+print("5**3=", 5**3)
+print("4**2=", 4**2)
+
+
+#Operadores de comparação
+
+print('')
+print('----------------------------------------------')
+print("Operadores de comparação")
+
+#declarando b = 2
+b = 2
+#conferindo se o valor de b é 2
+print(b==2)
+
+#mudando o valor de b para 3
+b=3
+#conferindo se o valor de b é 2
+print(b==2)
+
+#conferindo se o valor de b é diferente de 2
+print(b!=2)
