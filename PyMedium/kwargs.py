4ad66112088e342a49b5e0aff3e2f7bdb9c2e860
Luiz Felipe <luizfelipers19@gmail.com>
2021-06-07 22:59:16 -0300

    started kwargs arguments

---
+++ PyMedium/kwargs.py
@@ -0,0 +1,18 @@
+#args => argumentos
+
+#kwargs => keyword arguments
+
+#utilizando args normais
+def meuMetodo(arg1,arg2):
+    return arg1 + arg2
+
+print(meuMetodo(5,6))
+
+def meuMetodoLongo(arg1, arg2, arg3, arg4, arg5):
+    return arg1 + arg2 + arg3 + arg4 + arg5
+
+print(meuMetodoLongo(2,4,5,7,2))
+
+minhaListaSomada
+
+def soma_simplificada(  )
