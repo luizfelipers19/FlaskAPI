4e9b9ce4988d834eec06f1b45030e0b58595a1ac
LuizFelipe <luizfelipers19@gmail.com>
2021-06-09 21:47:48 -0300

    added a list of hotels, stored it in a variable to be returned when /hoteis endpoint is accessed

--- flaskinho/app.py
+++ flaskinho/app.py
@@ -4,9 +4,36 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+hoteis = [
+    {
+    'hotel_id':'alpha',
+    'nome': 'Alpha Hotel',
+    'estrelas': 4.3,
+    'diaria': 420.43,
+    'cidade':'Rio de Janeiro'
+
+    },
+    {
+    'hotel_id':'bravo',
+    'nome': 'Bravo Hotel',
+    'estrelas': 3.3,
+    'diaria': 220.99,
+    'cidade':'Florianopolis'
+
+    },
+    {
+    'hotel_id':'charlie',
+    'nome': 'Charlie Hotel',
+    'estrelas': 2.9,
+    'diaria': 150.49,
+    'cidade':'Monte Verde'
+
+    },
+]
+
 class Hoteis(Resource):
     def get(self):
-        return {'hoteis':'meus hoteis'}
+        return {'hoteis': hoteis}
 
 api.add_resource(Hoteis, '/hoteis')
 
