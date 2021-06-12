56ba84210b2318c5f55190e3e29869047bc33f4f
LuizFelipe <luizfelipers19@gmail.com>
2021-06-12 12:40:38 -0300

    added JWT support by installing flask_jwt_extended module, imported werkzeug.security module to use safe_str_cmp (safe string comparison) to compare the received password to the password saved in the server, and also created the UserLogin method and assigned the '/login' endpoint to it

--- flaskinho/app.py
+++ flaskinho/app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_restful import  Api
 from resources.hotel import Hoteis, Hotel
-from resources.usuario import Usuario, UserRegister
+from resources.usuario import Usuario, UserRegister, UserLogin
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
 api = Api(app)
+jwt = JWTManager(app)
+
 
 @app.before_first_request
 def cria_banco():
@@ -16,6 +20,8 @@ api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 api.add_resource(Usuario, '/usuarios/<int:user_id>')
 api.add_resource(UserRegister, '/cadastro')
+api.add_resource(UserLogin, '/login')
+
 
 if __name__ == '__main__':
     from sql_alchemy import banco
--- flaskinho/resources/usuario.py
+++ flaskinho/resources/usuario.py
@@ -1,7 +1,11 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
+from flask_jwt_extended import create_access_token
+from werkzeug.security import safe_str_cmp
 
-
+atributos = reqparse.RequestParser() #
+atributos.add_argument('login', type=str, required=True, help="O campo 'login' não pode ser deixado em branco!")
+atributos.add_argument('senha', type=str, required=True, help="O campo 'senha' não pode ser deixado em branco!")
 
 class Usuario(Resource):
     # /usuarios/{user_id}
@@ -22,9 +26,7 @@ class Usuario(Resource):
 class UserRegister(Resource):
     # /cadastro
     def post(self):
-        atributos = reqparse.RequestParser() #
-        atributos.add_argument('login', type=str, required=True, help="O campo 'login' não pode ser deixado em branco!")
-        atributos.add_argument('senha', type=str, required=True, help="O campo 'senha' não pode ser deixado em branco!")
+        
         dados = atributos.parse_args()
 
         if UserModel.find_by_login(dados['login']):
@@ -34,5 +36,16 @@ class UserRegister(Resource):
         user.save_user()
         return {"message":"Usuário com sucesso mlk"}, 201
 
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+
+        user = UserModel.find_by_login(dados['login'])
+        if user and safe_str_cmp(user.senha, dados['senha']):
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return {"access_token": token_de_acesso},200
+        return {"message":"The username or password is incorrect."}, 401 #Unauthorized
 
 
